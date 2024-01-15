75c734247fa332ae0aacba696e2099a4c7385a64
RafaelAmador16 <amadorcastellanorafaa@gmail.com>
2024-01-15 13:31:34 +0100

    Crud de sorteo

---
+++ src/Form/SorteoType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sorteo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SorteoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nombre')
+            ->add('fecha')
+            ->add('precio_papeleta')
+            ->add('cantidad_papeletas')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sorteo::class,
+        ]);
+    }
+}
